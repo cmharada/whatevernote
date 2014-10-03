2efd2646a53a0c9532f5f891a8263c2136f0d079
Corey Harada <haradacm@gmail.com>
2014-10-02 18:16:49 -0700

    More trying to fix bug

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,5 @@
 CKEDITOR.config.removePlugins = 'elementspath,resize';
 CKEDITOR.config.toolbarCanCollapse = 'true';
-CKEDITOR.config.language = 'en';
 CKEDITOR.config.width = "100%";
 CKEDITOR.config.height = "0";
 CKEDITOR.config.skin = 'bootstrapck';
@@ -9,6 +8,5 @@ CKEDITOR.config.toolbar = [
     { name: 'basicstyles', items: [ 'TextColor', 'Bold', 'Italic', 'Underline', 'Strike', 'Superscript', 'Subscript', 'RemoveFormat' ] },
     { name: 'align', items: [ 'Indent', 'Outdent', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
     { name: 'lists', items: [ 'NumberedList', 'BulletedList' ]},
-    { name: 'media', items: [ 'Checkbox', 'Image', 'Table', 'HorizontalRule'] },
-    { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo' ] }
+    { name: 'media', items: [ 'Checkbox', 'Image', 'Table', 'HorizontalRule'] }
 ];
